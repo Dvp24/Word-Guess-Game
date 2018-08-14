f88dd440740b0accdd5218abbec09d4e8e4b138c
Dhanashree <dvphalak@gmail.com>
2018-08-14 11:51:46 -0400

    not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,12 @@ win = 0;
 losses = 0;
 guess = 15;
 // Input 1: computer selecting a random word from an array
-computerOptionsArr = ["closer", "dont let me down", "roses", "paris"];
+computerOptionsArr = ["closer", "down", "roses", "paris"];
 function wordSel() {
   var computerChoice = computerOptionsArr[Math.floor(Math.random() * computerOptionsArr.length)];
   // console.log(computerChoice);
   return computerChoice;
 }
-document.getElementById("demo").innerHTML = wordSel();
 
 //condition1 : guess left condition check
 function guessLeft() {
@@ -24,7 +23,14 @@ document.onkeyup = function (event) {
   var userGuess = event.key;
   console.log(userGuess);
   document.getElementById("demo1").innerHTML = userGuess;
+  var check = wordSel().includes("userguess");
+  console.log(check);
+  
+// condition2 : checking letter in a word returns true if word has letter else false
+  if (check === "true") {
+    var n = wordSel().indexOf("useguess");
+    console.log("index = " + n);
+
+  }
 }
 
-var check = wordSel.includes("userguess");
-console.log(check);
