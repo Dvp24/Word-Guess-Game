28a4a673ac659758a0e3d1da87fe6bde8fd1ba91
Dhanashree <dvphalak@gmail.com>
2018-08-15 15:46:19 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,61 @@
 // setting variables
-win = 0;
-losses = 0;
-guess = 15;
+var win = 0;
+var losses = 0;
+var guess = 15;
+var hiddenWord = '';
+var guessesTillNow = [];
+var replaced = '';
+var userGuess;
+var lettr = '';
 // Input 1: computer selecting a random word from an array
-computerOptionsArr = ["closer", "down", "roses", "paris"];
+computerOptionsArr = ["closer", "dont let me down", "roses", "paris"];
+var word = '';
 function wordSel() {
   var computerChoice = computerOptionsArr[Math.floor(Math.random() * computerOptionsArr.length)];
-  // console.log(computerChoice);
   return computerChoice;
 }
-
-//condition1 : guess left condition check
-function guessLeft() {
-  guess--;
-  if (guess == 0) {
-    losses++;
-    wordSel();
-  }
+// printing --------- instead of word
+function printBlank() {
+  word = wordSel();
+  console.log("word : " + word);
+  console.log("length : " + word.length);
+  var blank = "-";
+  console.log(blank.repeat(word.length));
+  var hiddenWord = blank.repeat(word.length);
+  console.log(hiddenWord);
 }
-//input 2: user selects letter ////how to write all guesses in a row?????????
-document.onkeyup = function (event) {
-  var userGuess = event.key;
-  console.log(userGuess);
-  document.getElementById("demo1").innerHTML = userGuess;
-  var check = wordSel().includes("userguess");
-  console.log(check);
-  
-// condition2 : checking letter in a word returns true if word has letter else false
-  if (check === "true") {
-    var n = wordSel().indexOf("useguess");
-    console.log("index = " + n);
 
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+printBlank();
+if (guess > 0) {
+  document.onkeyup = function (event) {
+    userGuess = event.key;
+    document.getElementById("demo").innerHTML = "letter " + userGuess
+    console.log("letter " + userGuess);
+    guess--;
+    console.log("guess left " + guess);
+    guessesTillNow.push(userGuess);
+    console.log("till now " + guessesTillNow);
+    for (var i = 0; i < word.length; i++) {
+      var first = word.charAt(i);
+      if (first == userGuess) {
+        console.log("true");
+        lettr = hiddenWord[i];
+        replaced = hiddenWord.replace(lettr, userGuess);
+        console.log("new " + replaced);
+      }
+      else {
+        console.log("false try again");
+
+      }
+    }
   }
+
 }
+
+
+
+
+
 
--- assets/javascript/trial.js
+++
@@ -1,5 +0,0 @@
-// document.onkeyup = function (event) {
-//   var userGuess = event.key;
-//   console.log(userGuess);
-//   document.getElementById("demo1").innerHTML = userGuess;
-// }
